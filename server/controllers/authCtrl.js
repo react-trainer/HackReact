f3b6baeae2f239d25498cc832ad575cf58ff25a6
Ben <benjamindstidham@gmail.com>
2018-12-03 15:01:44 -0600

    server + auth0 complete

---
+++ server/controllers/authCtrl.js
@@ -0,0 +1,61 @@
+const { DOMAIN, CLIENT_ID, CLIENT_SECRET } = process.env;
+const passport = require("passport");
+const AuthStrategy = require("passport-auth0");
+
+module.exports = app => {
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  passport.use(
+    new AuthStrategy(
+      {
+        domain: DOMAIN,
+        clientID: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        callbackURL: "/login",
+        scope: "openid"
+      },
+      (accessToken, refreshToken, extraParams, profile, done) =>
+        done(null, profile)
+    )
+  );
+
+  passport.serializeUser((profile, done) => {
+    console.log(profile);
+    const db = app.get("db");
+    db.get_user_by_authid(profile.id).then(user => {
+      if (!user[0]) {
+        db.add_user_by_authid(profile.id)
+          .then(response => {
+            console.log(response);
+            return done(null, response[0]);
+          })
+          .catch(err => console.log(err));
+      } else {
+        return done(null, user[0]);
+      }
+    });
+  });
+
+  passport.deserializeUser((profile, done) => done(null, profile));
+
+  app.get(
+    "/login",
+    passport.authenticate("auth0", {
+      successRedirect: `${process.env.REACT_APP_CLIENT}/dashboard`,
+      failureRedirect: "/"
+      // failureFlash: true
+    })
+  );
+  function authenticated(req, res, next) {
+    if (req.user) {
+      next();
+    } else {
+      res.sendStatus(401);
+    }
+  }
+
+  app.get("/success", (req, res) => {
+    res.status(200).json(req.user);
+  });
+};
--- server/index.js
+++ server/index.js
@@ -1,3 +1,33 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const { SESSION_SECRET: secret, CONNECTION_STRING, PORT } = process.env;
+const port = PORT || 3100;
+const session = require("express-session");
+const massive = require("massive");
+const { json } = require("body-parser");
+const cors = require("cors");
+const authCtrl = require("./controllers/authCtrl");
+const masterRoutes = require("./masterRoutes");
+
+app.use(json());
+app.use(cors());
+app.use(
+  session({
+    secret,
+    saveUninitialized: true,
+    resave: false,
+    cookie: {
+      maxAge: 1209600000 // two weeks
+    }
+  })
+);
+
+massive(CONNECTION_STRING).then(dbInstance => {
+  app.set("db", dbInstance);
+});
+
+authCtrl(app);
+masterRoutes(app);
+
+app.listen(port, () => console.log(`Server now running on port ${port}`));
