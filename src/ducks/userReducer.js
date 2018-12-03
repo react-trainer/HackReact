2e5a1a13d07366b13a764b11a5e7f4fad0a39129
Ben <benjamindstidham@gmail.com>
2018-12-03 16:59:46 -0600

    structure change

--- src/ducks/userReducer.js
+++ src/ducks/userReducer.js
@@ -1,42 +1,40 @@
-import axios from 'axios';
+import axios from "axios";
 
 //action types
-const GET_USER = "GET_USER"
+const GET_USER = "GET_USER";
 
 //initial state
 
 const initialState = {
-    user: []
-}
+  user: []
+};
 
 //action creators
-export const getUser= () =>{
-    return{
+export const getUser = () => {
+  return {
     type: GET_USER,
-    payload: axios.get('/api/user')
-    }
-
-}
-
+    payload: axios.get("/api/user")
+  };
+};
 
 //reducer
 
-export default function userReducer(state = initialState, action){
-    switch(action.type){
-        case `${GET_USER}_PENDING`:
-            return{
-                ...state
-            }
-            case `${GET_USER}_FULFILLED`:
-            return{
-                ...state,
-                user: action.payload.data
-            }
-            case `${GET_USER}_REJECTED`:
-            return{
-                ...state
-            }
-            default:
-            return state
-}
+export default function userReducer(state = initialState, action) {
+  switch (action.type) {
+    case `${GET_USER}_PENDING`:
+      return {
+        ...state
+      };
+    case `${GET_USER}_FULFILLED`:
+      return {
+        ...state,
+        user: action.payload.data
+      };
+    case `${GET_USER}_REJECTED`:
+      return {
+        ...state
+      };
+    default:
+      return state;
+  }
 }
