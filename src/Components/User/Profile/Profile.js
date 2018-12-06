cec9173a599344e9c4c3657914b22b1cf7948227
T-BoneMadSkrilla <tylereftink@gmail.com>
2018-12-06 12:08:51 -0600

    got styling on the profile page pretty much how I want it

--- src/Components/User/Profile/Profile.js
+++ src/Components/User/Profile/Profile.js
@@ -15,26 +15,25 @@ import Nav from "../../Layout/Nav/Nav"
 
           <OuterContainer 
           justifyContent="flex-start" 
-        //   alignItems="baseline" 
+       
           backgroundColor="black" height='100vh'
           backgroundImage="linear-gradient(0deg, rgb(0,216,255)-158%, rgb(38, 38, 38)48%)" >
           <Nav/>
           <MainContainer>
 
           <MainContainer margin="3% 0 0 0"width="100vw">
-          <MainContainer width="30vw" >
-          <Card borderRadius="50%" objectFit="fill" backgroundImage="url(https://i.postimg.cc/YqCZc6f3/t-bone-linked.jpg)">
+          <MainContainer width="30vw" flexDirection="column">
+          <Card height="17vw"width="17vw" borderRadius="50%" objectFit="fill" backgroundImage="url(https://i.postimg.cc/YqCZc6f3/t-bone-linked.jpg)">
           </Card>
-          
+            <Button height="25px" width="100px">Edit</Button>
           </MainContainer>
 
-                     
-                     <Card>Login Streak</Card>
-                     <Card>Login Time </Card>
-                     <Card> Completed Lessons</Card>
+                     <Card backgroundColor="rgb(37,37,37)" boxShadow="null" borderRadius="null"width="29vw">about user</Card>
+                     <Card backgroundColor="rgb(37,37,37)" boxShadow="null" borderRadius="null"width="18vw">Login Streak</Card>
+                     <Card backgroundColor="rgb(37,37,37)" boxShadow="null" borderRadius="null"width="18vw">Login Time </Card>
+                     <Card backgroundColor="rgb(37,37,37)" boxShadow="null" borderRadius="null"width="18vw"> Completed Lessons</Card>
 
           </MainContainer>
-                       <Button >Edit</Button>
                        <MainContainer>
                            
                        </MainContainer>
