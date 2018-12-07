621c95021751fe76ad796446c00e47b74bd52462
James Sheehan <jmsdevx@gmail.com>
2018-12-07 14:02:45 -0600

    quiz modal

---
+++ src/Components/Dashboard/Lessons/CoreLessons/Core1/Quiz.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Quiz extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Quiz</h1>
+      </div>
+    );
+  }
+}
+
+export default Quiz;
