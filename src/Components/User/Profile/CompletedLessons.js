03c764d0b2bbf87d97d79b6a89276e2256639d67
T-BoneMadSkrilla <tylereftink@gmail.com>
2018-12-12 12:27:46 -0600

    profile is rendering data charts now

--- src/Components/User/Profile/CompletedLessons.js
+++ src/Components/User/Profile/CompletedLessons.js
@@ -1,15 +1,25 @@
 import React, {Component} from 'react';
+import {  RadialChart  } from 'react-vis';
 
+import './Profile.css'
 //use react vis instead of canvas, Tyler
 class CompletedLessons extends Component{
     render(){
+        const data = [
+                        {angle: 1, label: 'Not started'}, {angle: 5, label: 'Completed'}
+                      ];
+                    return( 
+                            
+                            <RadialChart data={data}
+                            width={250}
+                            height={250}/>
+                            
+                        
+                    )
+                }
+            }
         
-        return (
-            <div>
-                <canvas /> 
-            </div>
-        )
-    }
-}
 
 export default CompletedLessons
+
+
