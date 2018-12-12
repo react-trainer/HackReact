eca59c48d6023dc1bf54680601121c6d5050e2d9
T-BoneMadSkrilla <tylereftink@gmail.com>
2018-12-12 09:39:46 -0600

    made component for completedLessons

--- src/Components/User/Profile/CompletedLessons.js
+++ src/Components/User/Profile/CompletedLessons.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 
-
+//use react vis instead of canvas, Tyler
 class CompletedLessons extends Component{
     render(){
         
         return (
             <div>
-                <canvas />
+                <canvas /> 
             </div>
         )
     }
