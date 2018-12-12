4fb716f9aec7c8457df244ad2b2f4731f60491fc
Daniel De Hoyos <dsdehoyos@gmail.com>
2018-12-12 13:43:53 -0600

    changes in recent

--- src/Components/Dashboard/Recent/Recent.js
+++ src/Components/Dashboard/Recent/Recent.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { Card, MainContainer } from "../../resources/styles/masterStyles";
+import { connect } from "react-redux";
 
 class Recent extends Component {
   constructor() {
     super();
     this.state = {};
   }
+
   render() {
     return <MainContainer height="100vh">Recent</MainContainer>;
   }
